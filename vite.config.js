2d94cbae1eb51906817bcafc7d6029d2faa93114
Ivailo G. <ivailogramatikov05@gmail.com>
2024-12-12 14:53:32 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  base: "/global-trade/",
+  base: "",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
